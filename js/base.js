620005af10f7927b196a8e58ecfb6a26897a6f8a
Auric Vente <mprops@protonmail.com>
2023-12-02 23:56:37 -0600

    Update

--- js/base.js
+++ js/base.js
@@ -1,160 +1,160 @@
-const Flood = {}
-Flood.size = 14
-Flood.limit = 25
-Flood.started = false
-Flood.music_started = false
+const App = {}
+App.size = 14
+App.limit = 25
+App.started = false
+App.music_started = false
 
-Flood.init = function () {
-  Flood.el_grid = document.querySelector("#grid")
-  Flood.el_colorbox = document.querySelector("#colorbox")
-  Flood.el_counter = document.querySelector("#counter")
-  Flood.audio_laser = document.querySelector("#audio_laser")
-  Flood.audio_ambient = document.querySelector("#audio_ambient")
-  Flood.audio_bell = document.querySelector("#audio_bell")
-  Flood.get_style()
-  Flood.prepare_blocks()
-  Flood.prepare_colorbox()
-  Flood.prepare_counter()
-  Flood.start()
+App.init = () => {
+  App.el_grid = DOM.el(`#grid`)
+  App.el_colorbox = DOM.el(`#colorbox`)
+  App.el_counter = DOM.el(`#counter`)
+  App.audio_laser = DOM.el(`#audio_laser`)
+  App.audio_ambient = DOM.el(`#audio_ambient`)
+  App.audio_bell = DOM.el(`#audio_bell`)
+  App.get_style()
+  App.prepare_blocks()
+  App.prepare_colorbox()
+  App.prepare_counter()
+  App.start()
 }
 
-Flood.start = function () {
-  Flood.active = 0
-  Flood.count = 0
-  Flood.start_grid()
-  Flood.update_counter()
-  Flood.started = true
+App.start = () => {
+  App.active = 0
+  App.count = 0
+  App.start_grid()
+  App.update_counter()
+  App.started = true
 }
 
-Flood.random_int = function (min, max) {
+App.random_int = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
-Flood.get_style = function () {
+App.get_style = () => {
   let style = getComputedStyle(document.body)
 
-  Flood.colors = [
-    "#ffffff",
-    style.getPropertyValue("--color_1"),
-    style.getPropertyValue("--color_2"),
-    style.getPropertyValue("--color_3"),
-    style.getPropertyValue("--color_4"),
-    style.getPropertyValue("--color_5"),
-    style.getPropertyValue("--color_6")
+  App.colors = [
+    `#ffffff`,
+    style.getPropertyValue(`--color_1`),
+    style.getPropertyValue(`--color_2`),
+    style.getPropertyValue(`--color_3`),
+    style.getPropertyValue(`--color_4`),
+    style.getPropertyValue(`--color_5`),
+    style.getPropertyValue(`--color_6`)
   ]
 }
 
-Flood.set_active = function (item, color) {
+App.set_active = (item, color) => {
   item.active = true
-  Flood.active += 1
-  item.block.classList.add("active")
+  App.active += 1
+  item.block.classList.add(`active`)
 }
 
-Flood.set_color = function (item, color) {
+App.set_color = (item, color) => {
   item.color = color
   item.block.dataset.color = color
-  item.block.style.backgroundColor = Flood.colors[color]
+  item.block.style.backgroundColor = App.colors[color]
 }
 
-Flood.random_color = function () {
-  return Flood.random_int(1, Flood.colors.length - 1)
+App.random_color = () => {
+  return App.random_int(1, App.colors.length - 1)
 }
 
-Flood.start_grid = function () {
-  Flood.grid = []
-  Flood.el_grid.innerHTML = ""
+App.start_grid = () => {
+  App.grid = []
+  App.el_grid.innerHTML = ``
 
-  for (let y=0; y<Flood.size; y++) {
+  for (let y=0; y<App.size; y++) {
     let row = []
-    let elrow = document.createElement("div")
-    elrow.classList.add("grid_row")
+    let elrow = DOM.create(`div`)
+    elrow.classList.add(`grid_row`)
 
-    for (let x=0; x<Flood.size; x++) {
+    for (let x=0; x<App.size; x++) {
       let item = {}
       item.x = x
       item.y = y
       item.active = false
       item.checked = false
 
-      let color = Flood.random_color()
-      let block = document.createElement("div")
-      block.classList.add("block")
+      let color = App.random_color()
+      let block = DOM.create(`div`)
+      block.classList.add(`block`)
       block.dataset.color = color
       item.block = block
-      Flood.set_color(item, color)
+      App.set_color(item, color)
 
       row.push(item)
       elrow.append(block)
     }
 
-    Flood.grid.push(row)
-    Flood.el_grid.append(elrow)
+    App.grid.push(row)
+    App.el_grid.append(elrow)
   }
 
-  let first = Flood.grid[0][0]
-  Flood.set_active(first)
-  Flood.set_color(first, 0)
+  let first = App.grid[0][0]
+  App.set_active(first)
+  App.set_color(first, 0)
 }
 
-Flood.onclick = function (color) {
+App.onclick = (color) => {
   if (color === 0) {
     return
   }
-  
-  if (!Flood.music_started) {
-    Flood.audio_ambient.play()
-    Flood.music_started = true
+
+  if (!App.music_started) {
+    App.audio_ambient.play()
+    App.music_started = true
   }
 
-  Flood.fill(0, 0, color)
-  Flood.reset_checked()
-  Flood.count += 1
-  Flood.update_counter()
-  Flood.audio_laser.play()
+  App.fill(0, 0, color)
+  App.reset_checked()
+  App.count += 1
+  App.update_counter()
+  App.audio_laser.play()
 }
 
-Flood.prepare_blocks = function () {
-  Flood.el_grid.addEventListener("click", (e) => {
-    if (!Flood.started) {
+App.prepare_blocks = () => {
+  DOM.ev(App.el_grid, `click`, (e) => {
+    if (!App.started) {
       return
     }
 
-    if (e.target.classList.contains("block")) {
-      Flood.onclick(parseInt(e.target.dataset.color))
+    if (e.target.classList.contains(`block`)) {
+      App.onclick(parseInt(e.target.dataset.color))
     }
   })
 }
 
-Flood.prepare_colorbox = function () {
-  Flood.el_colorbox.addEventListener("click", (e) => {
-    if (!Flood.started) {
+App.prepare_colorbox = () => {
+  DOM.ev(App.el_colorbox, `click`, (e) => {
+    if (!App.started) {
       return
     }
 
-    if (e.target.classList.contains("block")) {
-      Flood.onclick(parseInt(e.target.dataset.color))
+    if (e.target.classList.contains(`block`)) {
+      App.onclick(parseInt(e.target.dataset.color))
     }
   })
 }
 
-Flood.reset_checked = function () {
-  for (let row of Flood.grid) {
+App.reset_checked = () => {
+  for (let row of App.grid) {
     for (let item of row) {
       item.checked = false
     }
   }
 }
 
-Flood.fill = function (y, x, color) {
-  if (y < 0 || y >= Flood.size) {
+App.fill = (y, x, color) => {
+  if (y < 0 || y >= App.size) {
     return
   }
 
-  if (x < 0 || x >= Flood.size) {
+  if (x < 0 || x >= App.size) {
     return
   }
 
-  let item = Flood.grid[y][x]
+  let item = App.grid[y][x]
 
   if (item.checked) {
     return
@@ -162,38 +162,38 @@ Flood.fill = function (y, x, color) {
 
   item.checked = true
   let is_first = y === 0 && x === 0
-  
+
   if (is_first || item.active || item.color === color) {
     if (!item.active) {
-      Flood.set_active(item, color)
+      App.set_active(item, color)
     }
 
     if (item.color !== color) {
-      Flood.set_color(item, color)
+      App.set_color(item, color)
     }
 
-    Flood.fill(y + 1, x, color)
-    Flood.fill(y - 1, x, color)
-    Flood.fill(y, x + 1, color)
-    Flood.fill(y, x - 1, color)
+    App.fill(y + 1, x, color)
+    App.fill(y - 1, x, color)
+    App.fill(y, x + 1, color)
+    App.fill(y, x - 1, color)
   }
 
   return
 }
 
-Flood.prepare_counter = function () {
-  Flood.el_counter.addEventListener("click", function () {
-    Flood.start()
+App.prepare_counter = () => {
+  DOM.ev(App.el_counter, `click`, () => {
+    App.start()
   })
 }
 
-Flood.update_counter = function () {
-  Flood.el_counter.textContent = `Clicks: ${Flood.count} / ${Flood.limit}`
-  let won = Flood.active === Flood.size * Flood.size
+App.update_counter = () => {
+  App.el_counter.textContent = `Clicks: ${App.count} / ${App.limit}`
+  let won = App.active === App.size * App.size
 
-  if (won || Flood.count >= Flood.limit) {
-    Flood.started = false
-    Flood.el_counter.textContent += ` (Click Here To Restart)`
-    Flood.audio_bell.play()
+  if (won || App.count >= App.limit) {
+    App.started = false
+    App.el_counter.textContent += ` (Click Here To Restart)`
+    App.audio_bell.play()
   }
 }
